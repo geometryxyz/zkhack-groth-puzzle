8d923c9ce460560483b8986e331c77208bf433a2
Kobi Gurkan <kobigurk@gmail.com>
2022-08-05 12:59:01 +0300

    fix: move task to right place

--- tasks/deploy-puzzle.ts
+++ tasks/deploy-puzzle.ts
@@ -3,10 +3,11 @@ import { task, types } from "hardhat/config"
 
 task("deploy:puzzle", "Deploy a Puzzle contract")
   .addOptionalParam<boolean>("logs", "Print the logs", true, types.boolean)
-  .setAction(async ({ logs }, { ethers }): Promise<Contract> => {
+  .setAction(async ({ logs }, hre): Promise<Contract> => {
 
-    const uri = "https://gateway.pinata.cloud/ipfs/QmYsrePrBhoWdxJD5oDSeuviCknQcfqoggDg6LkghaTw85";
-    const refundingGasPrice = BigInt("11000000000");
+    const ethers = hre.ethers;
+    const uri = "ipfs://QmNqAuUbFWUoQoyTWmzj1DGEEGnwgsQDZiMQLjtBpTp356";
+    const refundingGasPrice = BigInt("100000000000");
 
     const PuzzleFactory = await ethers.getContractFactory("Puzzle")
     const puzzle = await PuzzleFactory.deploy(uri, refundingGasPrice)
@@ -14,16 +15,21 @@ task("deploy:puzzle", "Deploy a Puzzle contract")
     logs && console.log(`Puzzle contract has been deployed to: ${puzzle.address}`)
 
     // put some eher at the beggining
-    let provider = new ethers.providers.InfuraProvider("kovan");
+    let provider = new ethers.providers.InfuraProvider("mainnet");
     const owner = new ethers.Wallet(`0x${process.env.BACKEND_PRIVATE_KEY}`, provider);
     let tx = {
       to: puzzle.address,
-      value: ethers.utils.parseEther('0.1'),
+      value: ethers.utils.parseEther('0.2'),
     };
 
     let receipt = await owner.sendTransaction(tx);
     await receipt.wait();
 
     logs && console.log(`Initial funds successfully sent`);
+
+    await hre.tenderly.persistArtifacts({
+	name: "Puzzle",
+	address: puzzle.address,
+    })
     return puzzle
   })
